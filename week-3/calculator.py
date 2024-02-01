0d9df1255ac27d091a6fb44d57e248144e49d42f
George <george_rtaylor@yahoo.com>
2024-01-31 23:44:33 -0600

    commit

---
+++ week-3/calculator.py
@@ -0,0 +1,20 @@
+first= int(input("First number: "))
+second = int(input("Second number: "))
+add = first + second
+print("Total: " + str(add))
+
+first= int(input("First number: "))
+second = int(input("Second number: "))
+Subtract = first - second
+print("Total: " + str(Subtract))
+
+first= int(input("First number: "))
+second = int(input("Second number: "))
+divide = first / second
+print("Total: " + str(divide))
+
+first= int(input("First number: "))
+second = int(input("Second number: "))
+multiply = first * second
+print("Total: " + str(multiply))
+
